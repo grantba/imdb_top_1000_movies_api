33b11f24559c730ac8e4fae5a360df3e59541918
Blaire <grant.blaire@gmail.com>
2021-08-01 18:41:36 -0500

    Updated routes.rb file including adding search feature to search OMDb's API for movie by name and movie by id.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :movie_watchlists
   resources :reviews
   resources :watchlists
   resources :movies
+
+  post '/search_by_name', to: 'movies#search_by_name'
+  post '/search_by_id', to: 'movies#search_by_id'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
