487c88de1e9ec7c53fb7530cec2e7b0bb2ca3359
Blaire <grant.blaire@gmail.com>
2021-08-01 18:45:54 -0500

    Added validations to review and watchlist models.

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -3,4 +3,6 @@ class Watchlist < ApplicationRecord
     has_many :movies, through: :movie_watchlists
     # belongs_to :user
     # dependent: :destroy
+
+    validates :category_type, presence: true, length: { maximum: 100, message: "- %{count} characters is the maximum allowed for a watchlist category." }
 end
