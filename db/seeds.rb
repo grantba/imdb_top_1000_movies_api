bc839e4a76d06043d5a725bf8b7d09e569d237aa
Blaire <grant.blaire@gmail.com>
2021-08-01 22:02:26 -0500

    Added seed data to seeds.rb file. Updated Gemfile.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,35 @@
 # Movie.all.each do |movie|
 #     Api.get_movie_by_id(movie.imdbID)
 # end
+
+Review.create(rating: 5, description: "Great Movie!", movie_id: 51) 
+Review.create(rating: 5, description: "One of the best movies of all time.", movie_id: 787)
+Review.create(rating: 5, description: "I could watch this movie again and again.", movie_id: 861)
+Review.create(rating: 3, description: "Hilarious.", movie_id: 574)
+Review.create(rating: 5, description: "Love This Movie.", movie_id: 764)
+Review.create(rating: 2, description: "Not So Great Movie.", movie_id: 232)
+Review.create(rating: 4, description: "Good Movie.", movie_id: 98)
+Review.create(rating: 1, description: "No Good.", movie_id: 270)
+Review.create(rating: 5, description: "Great Movie.", movie_id: 354)
+Review.create(rating: 5, description: "This is a great movie. I love this director.", movie_id: 430)
+
+Watchlist.create(category_type: "Favorites")
+Watchlist.create(category_type: "Need to Watch")
+Watchlist.create(category_type: "Comedy")
+Watchlist.create(category_type: "Classics")
+Watchlist.create(category_type: "Horror")
+Watchlist.create(category_type: "Never watch again")
+Watchlist.create(category_type: "Family")
+
+MovieWatchlist.create(movie_id: 51, watchlist_id: 1)
+MovieWatchlist.create(movie_id: 98, watchlist_id: 1)
+MovieWatchlist.create(movie_id: 430, watchlist_id: 1)
+MovieWatchlist.create(movie_id: 764, watchlist_id: 4)
+MovieWatchlist.create(movie_id: 574, watchlist_id: 3)
+MovieWatchlist.create(movie_id: 787, watchlist_id: 4)
+MovieWatchlist.create(movie_id: 861, watchlist_id: 4)
+MovieWatchlist.create(movie_id: 232, watchlist_id: 6)
+MovieWatchlist.create(movie_id: 270, watchlist_id: 6)
+MovieWatchlist.create(movie_id: 354, watchlist_id: 7)
+MovieWatchlist.create(movie_id: 118, watchlist_id: 2)
+
