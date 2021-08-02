a6bfd7a66dec8f57b6d03dfa0a219a5111585cc3
Blaire <grant.blaire@gmail.com>
2021-08-01 19:32:31 -0500

    Added web_scrapper.rb file to models folder to scrape the IMDb website using the kimurai gem. Added urls and logic to seeds.rb file to scrape the IMDb website for specific information and then use that information to make calls to the OMDb API to seed the database with movies.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# urls = ["https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&ref_=adv_prv", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=51&ref_=adv_nxt", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=101&ref_=adv_nxt", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=151&ref_=adv_nxt", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=201&ref_=adv_nxt", 
+# "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=251&ref_=adv_nxt", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=301&ref_=adv_nxt", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=351&ref_=adv_nxt", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=401&ref_=adv_nxt", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=451&ref_=adv_nxt", 
+# "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=501&ref_=adv_nxt", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=551&ref_=adv_nxt", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=601&ref_=adv_nxt", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=651&ref_=adv_nxt", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=701&ref_=adv_nxt",
+#  "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=751&ref_=adv_nxt", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=801&ref_=adv_nxt", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=851&ref_=adv_nxt", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=901&ref_=adv_nxt", "https://www.imdb.com/search/title/?groups=top_1000&sort=alpha,asc&start=951&ref_=adv_nxt"]
+# urls.each do |url|
+#    WebScrapper.parse!(:parse, url: url)
+#    WebScrapper.crawl!
+# end
+
+# Movie.all.each do |movie|
+#     Api.get_movie_by_id(movie.imdbID)
+# end
