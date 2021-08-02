c0b74742aa08466708faf27b757c207399f72065
Blaire <grant.blaire@gmail.com>
2021-08-01 19:44:32 -0500

    Deleted movie_watchlists_controller.rb file. Updated movies_controller.rb file to include implementing MoviesSerializer, adding :search to movie_params, and implementing specific error messages in create, update, destroy, search_by_name, and search_by_id actions.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,39 +3,62 @@ class MoviesController < ApplicationController
 
   # GET /movies
   def index
-    @movies = Movie.all
+    movies = Movie.all
 
-    render json: @movies
+    render json: MovieSerializer.new(movies)
   end
 
   # GET /movies/1
   def show
-    render json: @movie
+    render json: MovieSerializer.new(@movie)
   end
 
   # POST /movies
   def create
-    @movie = Movie.new(movie_params)
+    movie = Movie.new(movie_params)
 
-    if @movie.save
-      render json: @movie, status: :created, location: @movie
+    if movie.save
+      render json: MovieSerializer.new(movie)
     else
-      render json: @movie.errors, status: :unprocessable_entity
+      errors = movie.errors.map {|message| message.message}.join(", ")
+      render json: {errors: "#{errors}"}, status: :unprocessable_entity    
     end
   end
 
   # PATCH/PUT /movies/1
   def update
     if @movie.update(movie_params)
-      render json: @movie
+      render json: MovieSerializer.new(@movie)
     else
-      render json: @movie.errors, status: :unprocessable_entity
-    end
+      errors = @movie.errors.map {|message| message.message}.join(", ")
+      render json: {errors: "#{errors}"}, status: :unprocessable_entity    end
   end
 
   # DELETE /movies/1
   def destroy
-    @movie.destroy
+    if @movie.destroy
+      render json: {status: :ok}
+    else
+      render json: {errors: "There was an issue deleting this movie. Please try again."}      
+    end    
+  end
+
+  def search_by_name
+    @movie = Api.get_movie_by_name(params[:search])
+    if @movie == "That movie could not be found. Please try again." || @movie == "There was an issue loading this movie. Please try again."
+      render json: {errors: "There was either an issue loading this movie\n\nor that movie could not be found.\n\nPlease try again."}           
+    else
+      render json: MovieSerializer.new(@movie)
+    end
+  end
+
+  def search_by_id
+    @movie = Api.get_movie_by_id(params[:imdbID])
+    if @movie == "That movie could not be found. Please try again." || @movie == "There was an issue loading this movie. Please try again."
+      render json: {errors: "There was either an issue loading this movie\n\nor that movie could not be found.\n\nPlease try again."}           
+    else
+      render json: MovieSerializer.new(@movie)
+    end
   end
 
   private
@@ -46,6 +69,6 @@ class MoviesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def movie_params
-      params.require(:movie).permit(:Title, :Year, :Rated, :Released, :Runtime, :Genre, :Director, :Writer, :Actors, :Plot, :Awards, :Poster, :Ratings, :imdbRating, :imdbID, :BoxOffice, :Production, :Response)
+      params.require(:movie).permit(:Title, :Year, :Rated, :Released, :Runtime, :Genre, :Director, :Writer, :Actors, :Plot, :Awards, :Poster, :Ratings, :imdbRating, :imdbID, :BoxOffice, :Production, :Response, :search)
     end
 end
